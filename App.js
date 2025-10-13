62090ca504096cc6371d7d1a84d37f1308450094
juan lozano <oedoslozano@gmail.com>
2025-10-13 14:31:44 -0300

    .nada

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './LoginScreen';
+import HistoricoScreen from './HistoricoScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Historico" component={HistoricoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
